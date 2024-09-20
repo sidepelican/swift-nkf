9cc9c6462ea81a127a5bdfecffb4d7bff04a06a8
Iceman <okamura@qoncept.co.jp>
2024-09-20 12:52:24 +0900

    Use encoding option

--- Sources/Nkf/Nkf.swift
+++ Sources/Nkf/Nkf.swift
@@ -1,16 +1,20 @@
 import Foundation
 private import CNkf
 
-public enum Encoding: Character {
-    case iso2022JP = "j"
-    case shiftJIS = "s"
-    case eucJP = "e"
-    case utf8 = "w"
+public struct Encoding: CustomStringConvertible, Sendable {
+    public var description: String
+
+
+    public static let iso2022JP = Encoding(description: "ISO-2022-JP")
+    public static let shiftJIS = Encoding(description: "Shift_JIS")
+    public static let cp932 = Encoding(description: "Windows-31J")
+    public static let eucJP = Encoding(description: "EUC-JP")
+    public static let utf8 = Encoding(description: "UTF-8")
 }
 
 public enum Nkf {
     @NkfActor public static func convert(_ string: String, encoding: Encoding) -> Data? {
-        let options = "-m0 --ic=UTF-8 --oc=Windows-31J"
+        let options = "-m0 --ic=UTF-8 --oc=\(encoding)"
 
         var resultSize: Int32 = 0
         let resultBuf = options.withCString { optionsPtr in
